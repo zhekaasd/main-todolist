e84f7f5bba9e9dc3d4bf89600c56fd3a25ece01d
zhekaasd <zhekaasd13@gmail.com>
2020-09-13 09:28:30 +0400

    Updates

---
+++ precache-manifest.09e4711ed1cc622d968c1468993f3339.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d58b1012f7fcb6df21d9cedac200cd4",
+    "url": "/main-todolist/index.html"
+  },
+  {
+    "revision": "63041f618b245a8ae9a4",
+    "url": "/main-todolist/static/css/main.5ae3d9e0.chunk.css"
+  },
+  {
+    "revision": "f05844bd0ec8984f65fd",
+    "url": "/main-todolist/static/js/2.0fb95e8b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/main-todolist/static/js/2.0fb95e8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63041f618b245a8ae9a4",
+    "url": "/main-todolist/static/js/main.3c39e55d.chunk.js"
+  },
+  {
+    "revision": "3e82ed009096df151a8d",
+    "url": "/main-todolist/static/js/runtime-main.18adcae1.js"
+  }
+]);
